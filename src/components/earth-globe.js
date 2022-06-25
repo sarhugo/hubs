04be646f45d16c42b88415dd996f9a4ff33d5fd5
Hugo Sarti <sarhugo@gmail.com>
2022-06-24 22:43:03 -0300

    Atmosphere and locations display

--- src/components/earth-globe.js
+++ src/components/earth-globe.js
@@ -2,13 +2,82 @@ import { THREE } from "aframe";
 import { Vector3 } from "three";
 import { waitForDOMContentLoaded } from "../utils/async-utils";
 
+const GLOBE_RADIUS = 1;
+const pxPerDeg = (2 * Math.PI * GLOBE_RADIUS) / 360;
+
+const colorFn = x => {
+  return new THREE.Color().setHSL(0.6 - x * 0.5, 1.0, 0.5);
+};
+const fragmentShader = `
+uniform vec3 color;
+uniform float coefficient;
+uniform float power;
+varying vec3 vVertexNormal;
+varying vec3 vVertexWorldPosition;
+void main() {
+  vec3 worldCameraToVertex = vVertexWorldPosition - cameraPosition;
+  vec3 viewCameraToVertex	= (viewMatrix * vec4(worldCameraToVertex, 0.0)).xyz;
+  viewCameraToVertex = normalize(viewCameraToVertex);
+  float intensity	= pow(
+    coefficient + dot(vVertexNormal, viewCameraToVertex),
+    power
+  );
+  gl_FragColor = vec4(color, intensity);
+}`;
+
+const vertexShader = `
+varying vec3 vVertexWorldPosition;
+varying vec3 vVertexNormal;
+void main() {
+  vVertexNormal	= normalize(normalMatrix * normal);
+  vVertexWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+  gl_Position	= projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
+const createAtmosphereMesh = (geometry, options) => {
+  const atmosphereGeometry = geometry.clone();
+
+  // Resize vertex positions according to normals
+  const position = new Float32Array(geometry.attributes.position.count * 3);
+  for (let idx = 0, len = position.length; idx < len; idx++) {
+    const normal = geometry.attributes.normal.array[idx];
+    const curPos = geometry.attributes.position.array[idx];
+    position[idx] = curPos + normal * options.size;
+  }
+  atmosphereGeometry.setAttribute("position", new THREE.BufferAttribute(position, 3));
+  const atmosphereMaterial = new THREE.ShaderMaterial({
+    depthWrite: false,
+    fragmentShader,
+    transparent: true,
+    uniforms: {
+      coefficient: {
+        value: options.coefficient
+      },
+      color: {
+        value: new THREE.Color(options.color)
+      },
+      power: {
+        value: options.power
+      }
+    },
+    vertexShader
+  });
+  if (options.backside) {
+    atmosphereMaterial.side = THREE.BackSide;
+  }
+  return new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+};
+const getRandomAltitude = () => {
+  return Math.floor(Math.random() * 33) / 100;
+};
 AFRAME.registerComponent("earth-globe", {
   schema: {
     bumpMap: { type: "string", default: "" },
-    jsonDataUrl: { type: "string", default: "" }
+    jsonUrl: { type: "string", default: "" }
   },
   init() {
     const { mesh } = this.el.object3DMap;
+    mesh.rotation.y = Math.PI;
     if (this.data.bumpMap) {
       const loader = new THREE.TextureLoader();
       loader.loadAsync(this.data.bumpMap).then(texture => {
@@ -17,8 +86,16 @@ AFRAME.registerComponent("earth-globe", {
         mesh.material.needsUpdate = true;
       });
     }
+    const atmosphere = createAtmosphereMesh(mesh.geometry, {
+      backside: true,
+      color: "lightskyblue",
+      size: GLOBE_RADIUS * 0.25,
+      power: 3.5,
+      coefficient: 0.1
+    });
+    this.el.object3D.add(atmosphere);
     let leftHand, rightHand;
-
+    this.loadLocations();
     waitForDOMContentLoaded().then(() => {
       leftHand = document.getElementById("player-left-controller");
       rightHand = document.getElementById("player-right-controller");
@@ -50,5 +127,37 @@ AFRAME.registerComponent("earth-globe", {
     this.el.object3D.removeEventListener("interact", this.onGrabStart);
     this.el.object3D.removeEventListener("holdable-button-down", this.onGrabStart);
     this.el.object3D.removeEventListener("holdable-button-up", this.onGrabEnd);
+  },
+  loadLocations() {
+    this._baseGeometry = new THREE.BufferGeometry();
+    this._pointGeometry = new THREE.CylinderBufferGeometry(1, 1, 1, 12);
+    this._pointGeometry.applyMatrix4(new THREE.Matrix4().makeRotationX(Math.PI / 2));
+    this._pointGeometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0, -0.5));
+    this._pointMaterials = {
+      automat: new THREE.MeshBasicMaterial({ color: colorFn(0.9) }),
+      office: new THREE.MeshBasicMaterial({ color: colorFn(0.3) }),
+      officeWithAutomat: new THREE.MeshBasicMaterial({ color: colorFn(0.6) })
+    };
+    fetch(this.data.jsonUrl)
+      .then(response => response.json())
+      .then(locations => {
+        locations.forEach(location => {
+          this.addLocation(location);
+        });
+      });
+  },
+  addLocation(location) {
+    const point = new THREE.Mesh(this._pointGeometry);
+    const phi = ((90 - location.latitude) * Math.PI) / 180;
+    const theta = ((180 - location.longitude) * Math.PI) / 180;
+    point.position.x = GLOBE_RADIUS * Math.sin(phi) * Math.cos(theta);
+    point.position.y = GLOBE_RADIUS * Math.cos(phi);
+    point.position.z = GLOBE_RADIUS * Math.sin(phi) * Math.sin(theta);
+    point.lookAt(new THREE.Vector3(0, 0, 0));
+    point.scale.x = point.scale.y = 0.25 * pxPerDeg;
+    point.scale.z = Math.max(getRandomAltitude() * GLOBE_RADIUS, 0.1);
+    point.updateMatrix();
+    point.material = this._pointMaterials[location.type];
+    this.el.object3D.add(point);
   }
 });
