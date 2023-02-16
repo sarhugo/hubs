6d88ac601765e0a8d1eb78bcc7edeb7a5087b41d
Hugo Sarti <sarhugo@gmail.com>
2023-02-16 15:30:42 -0300

    Fix annoying error message

--- src/change-hub.js
+++ src/change-hub.js
@@ -113,7 +113,7 @@ window.addEventListener("popstate", function () {
   if (!APP.store.state.preferences.fastRoomSwitching) return;
   const qs = new URLSearchParams(location.search);
   const newHubId = qs.get("hub_id") || document.location.pathname.substring(1).split("/")[0];
-  if (newHubId !== APP.hub.hub_id) {
+  if (newHubId !== APP.hub?.hub_id) {
     changeHub(newHubId, false);
   }
 });
